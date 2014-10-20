48ec72b4b7480ed80f28ad616f677790004ef202
DaschnerJ <daschner02@aol.com>
2014-10-19 20:23:06 -0400

    Update

--- src/main/java/net/playmymc/daschner/justin/items/ItemTestIngot.java
+++ src/main/java/net/playmymc/daschner/justin/items/ItemTestIngot.java
@@ -3,7 +3,7 @@ package net.playmymc.daschner.justin.items;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.playmymc.daschner.justin.reference.reference;
-
+//test item
 public class ItemTestIngot extends Item 
 {
 	public ItemTestIngot()
